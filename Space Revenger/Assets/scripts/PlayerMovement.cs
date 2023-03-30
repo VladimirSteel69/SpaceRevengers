c6694e717928ed463f7319cb50f4a7e4831eed92
TudorBan <129309327+TudorBan@users.noreply.github.com>
2023-03-30 21:14:48 +0300

    movement update

--- Space Revenger/Assets/scripts/PlayerMovement.cs
+++ Space Revenger/Assets/scripts/PlayerMovement.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 300f;
-    
-    float movement = 0f;
+    public float speed = 200f; //variabila de viteza
 
-    // Update is called once per frame
     void Update()
     {
-        movement = Input.GetAxisRaw("Horizontal");
+        //aici nu am pus nimic deocamdata
     }
 
     void FixedUpdate()
     {
-        transform.RotateAround(Vector3.zero, Vector3.forward, -movement * speed * Time.deltaTime);
+        if (Input.touchCount > 0) //monitorizare atingerilor(in cazul in care sunt mai multe)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)    //se verifica daca ecranul este apsat
+            {
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.WorldToScreenPoint(transform.position).z)); //converteste coordonatele din lumea din unity in pixeli pe ecran
+                if (touch.position.x < Screen.width/2.0f) //verifica daca atingerea este in partea stanga a ecranului
+                {
+                    transform.RotateAround(Vector3.zero, Vector3.forward, speed * Time.fixedDeltaTime); //rotirea spre stanga
+                }
+                else if (touch.position.x > Screen.width/2.0f) //verifica daca atingerea este in partea dreapta a ecranului
+                {
+                    transform.RotateAround(Vector3.zero, Vector3.forward, -speed * Time.fixedDeltaTime); //rotirea spre dreapta
+                }
+            }
+        }
     }
 
-    
-    
 }
