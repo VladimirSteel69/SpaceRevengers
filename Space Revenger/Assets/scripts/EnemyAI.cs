2c0e5a33a61a3c8698ab571582cada215819484b
TudorBan <129309327+TudorBan@users.noreply.github.com>
2023-03-31 15:27:05 +0300

    Enemy AI

---
+++ Space Revenger/Assets/scripts/EnemyAI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.position - rb.position;
+
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Destroy(gameObject);
+    }
+}
